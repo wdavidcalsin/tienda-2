fafa0b5cc18c9cdaa608bedd20868387b91f14c7
Wdavid Calsin <david2c142@gmail.com>
2020-11-19 21:25:15 -0500

    wishles avanced 2

--- src/views/home.tsx
+++ src/views/home.tsx
@@ -1,26 +1,17 @@
 import React, { Fragment } from "react";
 import Cards from "../components/cards";
 // import _ from "lodash";
-import { useWishles } from "../Context/wishles-context";
 import CountImage from "../api/descriptionImg.json";
 
 function Home() {
-   const { value_, setValue } = useWishles();
-
-   const aumentar = () => {
-      setValue(value_ + 1);
-   };
-
    return (
       <div className="section-home">
          <div className="container">
             {CountImage.map((item: any) => (
                <Fragment>
                   <Cards title={item.title} />
-                  <h2>{item.id}</h2>
                </Fragment>
             ))}
-            <button onClick={aumentar}> +</button>
          </div>
       </div>
    );
